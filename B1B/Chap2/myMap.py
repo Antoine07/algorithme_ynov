894f1817920c40347733b53e7d0c15ec91ff6523
Antoine <antoine.lucsko@gmail.com>
2020-03-20 17:45:50 +0100

    function comme valeur

--- B1B/Chap2/myMap.py
+++ B1B/Chap2/myMap.py
@@ -3,10 +3,22 @@
 def myMap(l, func):
 
     for i in range(len(l)):
-        l[i] = func(l[i])
+        # on peut appliquer un calcul sur chaque élément de notre liste
+        # avec une fonction qui n'est pas encore définie
+        l[i] = func( l[i] )
 
     return l
 
 def square(x):
 
     return x**2
+
+print([1,2,3,4])
+
+print( myMap( [1,2,3,4] , square) )
+
+def augmentation(x):
+
+    return x + x*0.5
+
+print( myMap( [1,2,3,4] , augmentation) )
