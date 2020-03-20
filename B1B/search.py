7888a0fa50d01622aa3012898b132301a0879362
Antoine <antoine.lucsko@gmail.com>
2020-03-20 17:01:38 +0100

    search v2

--- B1B/search.py
+++ B1B/search.py
@@ -1,6 +1,6 @@
 
 
-def search(n, l):
+def search_v1(n, l):
 
     for i in range(len(l)):
         # print(l[i])
@@ -10,4 +10,16 @@ def search(n, l):
     return False
 
 
-print( search(7, [8,9,7,10]) )
+print( search_v1(7, [8,9,7,10]) )
+
+
+def search_v2(n, l):
+
+    for num in l:
+        # print(num)
+        if num == n:
+            return True
+
+    return False
+
+print( search_v2(71, [8,9,7,10]) )
