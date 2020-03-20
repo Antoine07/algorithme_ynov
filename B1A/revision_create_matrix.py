78d48e786cce4a7a9707b057b6036104ac6f3a74
Antoine <antoine.lucsko@gmail.com>
2020-03-20 10:20:15 +0100

    debut algo transpose

---
+++ B1A/revision_create_matrix.py
@@ -0,0 +1,67 @@
+
+# l : lignes et c : colonnes
+def create_matrix_show(l, c, v):
+
+    for _ in range(l):
+        for _ in range(c):
+            print(v, end="")
+        print()
+
+# create_matrix_show(5, 3, 0)
+
+def create_matrix(l, c, v):
+
+    # création d'une liste
+    m = []
+    for _ in range(l):
+        row = []
+        for _ in range(c):
+            row.append(v)
+        m.append(row)
+
+    return m
+
+A = create_matrix(5, 3, 0)
+# print(A)
+
+
+"""
+A = [
+    [1,2,3],
+    [1,2,3],
+    [1,2,3],
+    [1,2,3],
+]
+
+4 X 3
+
+"""
+
+B = [
+    [1,2,3], # 0
+    [1,2,3], # 1  <--
+    [1,2,3], # 2
+    [1,2,3], # 3
+    [1,2, 3], # 4
+]
+
+def dimension(m):
+    l = len(m)
+    c = len(m[0])
+
+    # on vérifie la cohérence des dimensions
+    # c'est dire ici que toutes les lignes on bien la même dimension
+    for i in range(1, l):
+    # assert arrête le programme 
+       assert c == len(m[i]) 
+
+    # En Python on peut retourner deux valeurs dans le script, on renvoit un tuple Python
+    # un tuple c'est une structure de données comme une liste par exemple
+    return (l, c)
+
+print( type( dimension(B) ) )
+
+# a et b
+# a, b, c = 12, 8, 11
+# assigner les valeur retourner par la fonction dimension pour a, b
+a, b = dimension(B)
