bd3929bd145383100e0c87c21593bad2d78af841
Antoine <antoine.lucsko@gmail.com>
2020-03-23 14:31:15 +0100

    max probleme pour demain

--- B1B/Chap2/max.py
+++ B1B/Chap2/max.py
@@ -23,3 +23,16 @@ def max4(u,v,w, p):
     return max2(u, max2(v, max2(w,p)))
 
 print(max4(1000,200,30, 4))
+
+# Comme exercice essayez généraliser avec une fonction maxGen
+
+#unpacking 
+def maxGen( *numbers ):
+
+    print(numbers)
+
+    for number in numbers:
+        print(number)
+
+maxGen(1,2,3,4,5,6,7,8,9)
+maxGen(1,2,3)
